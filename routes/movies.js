1315213ebb9373719c5ab79b9033ace678618812
Tobias Gerschner <tobias.gerschner at gmail dot com>
2016-08-06 22:29:52 +1200

    testing something

--- routes/movies.js
+++ routes/movies.js
@@ -7,14 +7,18 @@ var AWS = require('aws-sdk'),
 router.get('/', function(req, res, next) {
     var params = {
         TableName: 'Movies'
-    }
+    };
+
+    console.log("request", req);
+    console.log("resource", res);
     var docClient = new DOC.DynamoDB(new AWS.DynamoDB({region: 'ap-southeast-2' }));
     // var docClient = new DOC.DynamoDB(new AWS.DynamoDB({region: 'ap-southeast-2', endpoint: new AWS.Endpoint("http://localhost:8000")}));
-    docClient.scan(params, function(err, data) {
-        if (err) console.log(err);
-        else res.send(data);
-    });
+    // docClient.scan(params, function(err, data) {
+    //     if (err) console.log(err);
+    //     else res.send(data);
+    // });
 
+    res.render('index', { title: JSON.stringify(res, null, 2)});
 });
 
 module.exports = router;
